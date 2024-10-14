734905a5e4dececb964d5ef51065488efd0ae2ef
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2024-10-14 19:48:09 -0300

    v-for: iteração no Vue

--- treino-vue-pic/src/App.vue
+++ treino-vue-pic/src/App.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
+
+    <ul>
+      <!-- OBS.: Além do v-for="foto of fotos", também podemos utilizar o v-for="foto in fotos" -->
+      <!-- Quando usamos "of", estamos mais próximos da sintaxe dos iterators em JavaScript. -->
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+
+    <!-- Atalho para criar a esturura abaixo. Digite no VSCode "ul>li*2>img", clica "Ctrl+Espaço" e selecione a opção com "Emmet Abbreviation" -->
+    <!-- <ul>
+      <li>
+        <img :src="foto1.url" :alt="foto1.titulo">
+      </li>
+      <li>
+        <img :src="foto2.url" :alt="foto2.titulo">
+      </li>
+    </ul> -->
+
     <!-- O problema do uso do "v-bind", é que o elemento vai ficar bastante "verboso", imagina colocar 4 atributos,
      teríamos 4 "v-binds". Imaginando isso, o "Vue" tem um "atalho" para não precisar ficar colocando o "v-bind", 
      sendo que apenas colocando ":" na frente do atributo, tem o mesmo efeito de ter o "v-bind". -->
-    <img :src="foto.url" :alt="foto.titulo" />
+    <!-- <img :src="foto.url" :alt="foto.titulo" /> -->
 
     <!-- Para fazer com que as informação sejam utilizadas no atributos do elemento, precisamos utilizar diretiva,
      como o "v-bind", dessa forma o vue "ensina" ao navegador como deve ser renderizado as informações. -->
@@ -31,10 +50,28 @@ export default {
   data() {
     return {
       titulo: 'Treino Vue Pic',
-      foto: {
-        url: 'https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png',
-        titulo: 'Cachorro'
-      }
+      fotos: [
+        {
+          url: 'https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png',
+          titulo: 'Cachorro'
+        },
+        {
+          url: 'https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png',
+          titulo: 'Cachorrão'
+        },
+        {
+          url: 'https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png',
+          titulo: 'Cachorrinho'
+        }
+      ]
+      // foto1: {
+      //   url: 'https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png',
+      //   titulo: 'Cachorro'
+      // },
+      // foto2: {
+      //   url: 'https://love.doghero.com.br/wp-content/uploads/2018/12/golden-retriever-1.png',
+      //   titulo: 'Cachorrão'
+      // }
     }
   }
 }
