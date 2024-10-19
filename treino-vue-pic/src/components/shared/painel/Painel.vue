241d8204c8e4b251c87eda549725f0fdd6cc6d87
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2024-10-19 10:14:21 -0300

    Ajuste para que o estilo seja aplicado apenas no escopo do componente

--- treino-vue-pic/src/components/shared/painel/Painel.vue
+++ treino-vue-pic/src/components/shared/painel/Painel.vue
@@ -13,8 +13,12 @@ export default {
     props: ['titulo']
 }
 </script>
-  
-<style>
+
+<!-- Para que o estilo descrito abaixo afeta apenas o "template" do componente, colocamos o atributo "scoped", -->
+<!--  como isso, o Vue vai aplicar esse estilo apenas dentro desse componente, ou seja, caso outros componentes -->
+<!--  tenham definido a mesma classe css (por exemplo, "painel"), o estilo definido nesse componente não afetará -->
+<!--  o estilo definido por outros componenente com o mesmo nome. -->
+<style scoped>
     /* Estilo do painel */
     .painel {
         padding: 0 auto;
@@ -35,6 +39,14 @@ export default {
         margin: 0 0 15px 0;
         padding: 10px;
         text-transform: uppercase;
+    }
+
+    /* Estamos utilizando o seletor universal (*), ou seja, o estilo escrito dentro desse seletor */
+    /*  será aplicado a todos os elementos da página, ou seja, com o escopo global. */
+    /* Mas com a inclusão do atributo "scoped" na tag "style", esse seletor universal vai modificar */
+    /*  apenas o template desse componente. */
+    * {
+        box-shadow: 5px 5px 5px;
     }
 </style>
   
